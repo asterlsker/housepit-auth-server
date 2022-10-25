87aee04f531151eafdd4d1e23d64e84c2184056a
jungho.baek <jungho.baek@42dot.ai>
2022-10-25 14:53:47 +0900

    [chore] 주석 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-    // kotlin library
+    // Kotlin library
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
